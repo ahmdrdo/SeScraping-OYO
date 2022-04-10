1fbe9a8294081f9827e8377b55a737e870638d0c
Ahmad Ridho <ahmdrdo@gmail.com>
2022-04-10 18:03:36 +0700

    Finalizing script scrapeOyo.py

--- scrapeOyo.py
+++ scrapeOyo.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 
 opt = webdriver.ChromeOptions()
@@ -15,7 +16,7 @@ opt.add_argument("--disable-notifications") # Disable browser push notification
 opt.add_experimental_option("excludeSwitches", ["enable-logging"])
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=opt)
-driver.implicitly_wait(30)
+driver.implicitly_wait(15)
 driver.get('https://www.oyorooms.com/id/hotels-in-yogyakarta/')
 driver.maximize_window()
 
@@ -26,7 +27,7 @@ page_url = [url.get_attribute('href') for url in driver.find_elements(By.CLASS_N
 hotel_url = []
 for page in page_url:
     driver.get(page)
-    hotel_url.extend(url.get_attribute('href') for url in driver.find_elements(By.CSS_SELECTOR, 'a.c-nn640c'))
+    hotel_url.extend(url.get_attribute('href') for url in driver.find_elements(By.CSS_SELECTOR, 'div.listingHotelDescription__contentWrapper--left > a.c-nn640c'))
 
 hotel_name = []
 location = []
@@ -34,15 +35,20 @@ rate = []
 facility = []
 category = []
 # Scrape hotel info, page by page
-for hotel in hotel_url[:5]:
-    driver.get(hotel)
-    time.sleep(2) # Expect delay on loading page
+for url in hotel_url:
+    driver.get(url)
+    print('Scraping... ' + str(hotel_url.index(url)+1) + ' of ' + str(len(hotel_url)) + " hotels")
     hotel_name.append(driver.find_element(By.CSS_SELECTOR, 'h1.c-1wj1luj').text)
     location.append(driver.find_element(By.CSS_SELECTOR, '[itemprop="streetAddress"]').text)
-    rate.append(float(driver.find_element(By.CSS_SELECTOR, 'span.c-1uxth7l').text))
     facility.append(', '.join([i.text for i in driver.find_elements(By.CSS_SELECTOR, 'div.c-12w6zty')]))
     category.append(', '.join([i.text for i in driver.find_elements(By.CSS_SELECTOR, 'span.c-2j9z2q')]))
+    # Some hotels are unrated yet/NEW, there for:
+    try:
+        rate.append(driver.find_element(By.CSS_SELECTOR, 'div.c-186t5ao').text)
+    except NoSuchElementException:
+        rate.append('NEW')
 
+# Compile info as dataframe
 hotel_df = pd.DataFrame({
     'hotel_name': hotel_name,
     'location': location,
