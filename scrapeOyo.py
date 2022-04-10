c815694bf625eeccc2fc60dc9b30651e2a8d20eb
Ahmad Ridho <ahmdrdo@gmail.com>
2022-04-10 15:12:29 +0700

    Update scrape.py

--- scrapeOyo.py
+++ scrapeOyo.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 import pandas as pd
 import time
 from selenium import webdriver
@@ -21,8 +22,34 @@ driver.maximize_window()
 # Get url for all pages (href attributes)
 page_url = [url.get_attribute('href') for url in driver.find_elements(By.CLASS_NAME, 'ListingPagination__pageContainer--page')]
 
+# Get url for all hotels
+hotel_url = []
 for page in page_url:
     driver.get(page)
+    hotel_url.extend(url.get_attribute('href') for url in driver.find_elements(By.CSS_SELECTOR, 'a.c-nn640c'))
+
+hotel_name = []
+location = []
+rate = []
+facility = []
+category = []
+# Scrape hotel info, page by page
+for hotel in hotel_url:
+    driver.get(hotel)
+    time.sleep(2) # Expect delay on loading page
+    hotel_name.append(driver.find_element(By.CSS_SELECTOR, 'h1.c-1wj1luj').text)
+    location.append(driver.find_element(By.CSS_SELECTOR, '[itemprop="streetAddress"]').text)
+    rate.append(float(driver.find_element(By.CSS_SELECTOR, 'span.c-1uxth7l').text))
+    facility.append(', '.join([i.text for i in driver.find_elements(By.CSS_SELECTOR, 'div.c-12w6zty')]))
+    category.append(', '.join([i.text for i in driver.find_elements(By.CSS_SELECTOR, 'span.c-2j9z2q')]))
+
+hotel_df = pd.DataFrame({
+    'hotel_name': hotel_name,
+    'location': location,
+    'rate': rate,
+    'facility': facility,
+    'category': category
+})
 
 driver.close()
 driver.quit()
